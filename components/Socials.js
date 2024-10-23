0940a0229997f258af6e71acc245b02d26146c15
Sourjya-Saha <sourjya.saha76@gmail.com>
2024-10-23 13:05:03 +0530

    commit

--- components/Socials.js
+++ components/Socials.js
@@ -4,10 +4,10 @@ import { FaGithub , FaInstagram , FaLinkedin , FaTwitter } from 'react-icons/fa'
 
 const Socials = () => {
   return( <div className="flex items-center gap-x-5 text-lg">
-    <Link href={''} className=" text-2xl hover:text-accent transition-all duration-300"><FaLinkedin /></Link>
-    <Link href={''} className="text-2xl hover:text-accent transition-all duration-300"><FaInstagram /></Link>
-    <Link href={''} className="text-2xl hover:text-accent transition-all duration-300"><FaGithub /></Link>
-    <Link href={''} className="text-2xl hover:text-accent transition-all duration-300"><FaTwitter /></Link>
+    <Link href={'https://www.linkedin.com/in/sourjya-saha-106b42317/'} className=" text-2xl hover:text-accent transition-all duration-300"><FaLinkedin /></Link>
+    <Link href={'https://www.instagram.com/_.sourjya._/?next=%2F'} className="text-2xl hover:text-accent transition-all duration-300"><FaInstagram /></Link>
+    <Link href={'https://github.com/Sourjya-Saha'} className="text-2xl hover:text-accent transition-all duration-300"><FaGithub /></Link>
+    <Link href={'https://x.com/SahaSourjya'} className="text-2xl hover:text-accent transition-all duration-300"><FaTwitter /></Link>
   </div>
   );
 };
